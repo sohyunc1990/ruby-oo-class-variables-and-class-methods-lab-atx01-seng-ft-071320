df8de513249f30dce08b1ee4ede2dfbe5b3e6528
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-19 01:01:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,14 @@ require 'pry'
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@artists = []
   
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << artist
   end
 
 end
